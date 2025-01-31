851ddd3af95dae4365b86f89ccf0aa9f840b961e
Nick Richards <vrm.richards@gmail.com>
2025-01-31 00:07:21 -0700

    adding basic footer component

---
+++ devportfolio/src/app/componants/footer.js
@@ -0,0 +1,10 @@
+function footer() {
+    return (
+        <div>
+            <p>Email</p>
+            <p>Github</p>
+            <p>LinkedIn</p>
+            <p>Coded in Visual Studio Code(VS Code). Built with Next.js and Tailwind CSS, hosted and deployed with Vercel.</p>
+        </div>
+    )
+}
