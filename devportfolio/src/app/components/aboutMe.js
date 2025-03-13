654e1e3e3eee7e532a6127b66b803e731ce34834
Nr1cha <vrm.richards@gmail.com>
2025-03-12 19:27:22 -0600

    adding icons and color changes

--- devportfolio/src/app/components/aboutMe.js
+++ devportfolio/src/app/components/aboutMe.js
@@ -1,15 +1,22 @@
+'use client'
+import React from 'react';
+import {
+    LinkedinIcon,
+    GithubIcon
+} from 'next-share';
 export default function About() {
     return (
         <>
-            <h1 className="text-4xl font-bold text-gray-800">Nick Richards</h1>
+            <h1 className="text-4xl font-bold text-gray-800 nameTitle">Nick Richards</h1>
             <h2>Junior Web Developer</h2>
-            <p>I build full stack accessable projects on the web</p>
+            <p className="subText">I build full stack accessable projects on the web</p>
 
             {/* socials */}
-            <ul>
-                <li><a href="https://github.com/Nr1cha">Github</a></li>
-                <li><a href="https://www.linkedin.com/in/nickr1cha/">LinkedIn</a></li>
+            <ul className='socials'>
+                <li><GithubIcon url={'https://github.com/Nr1cha'}size={32} round/></li>
+                <li><LinkedinIcon url={'https://www.linkedin.com/in/nickr1cha/'} size={32} round /></li>
             </ul>
+
         </>
 
     )
