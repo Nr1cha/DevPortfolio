4984cf929282442ce713ac4a932f0699c85bcaec
Nr1cha <vrm.richards@gmail.com>
2025-03-15 01:25:25 -0600

    WIN adding basic styling and placeholder text

--- devportfolio/src/app/components/projects.js
+++ devportfolio/src/app/components/projects.js
@@ -4,6 +4,8 @@ export default function Projects() {
   return (
     <div>
       <p>this is a test</p>
+      <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Cubilia justo dis vestibulum vitae sagittis himenaeos turpis. Rhoncus per finibus in neque quis; nullam erat accumsan. Auctor consectetur fusce purus maecenas fringilla.</p>
+
       <ul>
         <li></li>
         <li></li>
