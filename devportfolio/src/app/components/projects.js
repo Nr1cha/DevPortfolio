3a5325e4bb9a89331d0266276d5c7b49763fcc21
Nick Richards <vrm.richards@gmail.com>
2025-04-22 21:28:14 -0600

    changed text color for later darkmode

--- devportfolio/src/app/components/projects.js
+++ devportfolio/src/app/components/projects.js
@@ -1,16 +1,33 @@
 "use client";
+
+
+function ListItem(props) {
+  return <li>{props.projectItem}</li>
+}
+
 // list out projects or assignments here
 export default function Projects() {
+
+  const projects = ['project1', 'project2', 'project3']
+
+  let elements = []
+
+  for(let i = 0; i < projects.length; i++) {
+    const projectItem = projects[i]
+    
+    elements.push(
+      <ListItem key={i} projectItem={projectItem} />
+    )
+  }
+
   return (
+
     <div>
       <p>this is a test</p>
       <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Cubilia justo dis vestibulum vitae sagittis himenaeos turpis. Rhoncus per finibus in neque quis; nullam erat accumsan. Auctor consectetur fusce purus maecenas fringilla.</p>
 
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        {elements}
       </ul>
     </div>
   );
